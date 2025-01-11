1fd71a34271168622b457e17653117c96a21344e
Rahul Mewada <rahulmewada659@gmail.com>
2025-01-11 20:46:03 +0530

    updating cookie access command

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        console.log("cookie", req.headers.cookie.split("=")[1]);
+        const token = req.headers.cookie.split("=")[1];
         if(!token) {
             return res.status(401).json({
                 "message":"User not authenticated"
