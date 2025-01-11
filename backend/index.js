8c9d64168186e92f3d4eae223bd8713b2206dd91
Rahul Mewada <rahulmewada659@gmail.com>
2025-01-11 22:24:34 +0530

    cross origin changed to deployed frontend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://gmail-clone-frontend-six.vercel.app',
     credentials: true
 };
 app.use(cors(corsOptions));
